5a0fdf03b8f1dab59ed352d73281d7eb41782340
Yuri Sakai <yusasabi@gmail.com>
2026-01-20 12:57:10 +0900

    TypeScriptエラー修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,14 @@
-import { DeployButton } from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import { AuthButton } from "@/components/auth-button";
 import { Hero } from "@/components/hero";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-steps";
-import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
 import { Suspense } from "react";
 import SectionBlock from "@/components/ui/sections";
 import SkillsContent from "@/components/skills/skills-content";
 import getDataFromSupabase from "@/lib/getData";
 import WorksGrid from "@/components/works/works-grid";
+import { workType } from "@/types/job.types";
 
 async function WorksData() {
-  const works = await getDataFromSupabase("works");
+  const works = await getDataFromSupabase<workType>("works");
 
   return <WorksGrid works={works}></WorksGrid>;
 }
