a35f0745b7a0f99ddb49bc7d6d26e9eb6c6d837e
Yuri Sakai <yusasabi@gmail.com>
2026-01-20 20:34:32 +0900

    work詳細ページ作成

--- app/works/[workSlug]/page.tsx
+++ app/works/[workSlug]/page.tsx
@@ -1,19 +1,40 @@
 import { Suspense } from "react";
+import { getEachDataFromSupabase } from "@/lib/getData";
+import { workType } from "@/types/job.types";
+import Image from "next/image";
 
-type params = {
-  workSlug: string
+type PageProps = {
+  params: Promise<{
+    workSlug: string;
+  }>;
+};
+
+async function WorkData({ params }: PageProps) {
+  const { workSlug } = await params;
+  const data = await getEachDataFromSupabase<workType>("works", workSlug);
+
+  if (!data) {
+    return <div>作品が見つかりませんでした</div>;
+  }
+
+  return (
+    <div>
+      <h2>{data.title}</h2>
+      <p>{data.description}</p>
+      <Image src={`/images/screenshots/${data.slug}.webp`} alt={data.title} width={1000} height={1000} />
+      <p>{data.role}</p>
+      <p>{data.skills}</p>
+      <p><a href={data.url || ""} target="_blank">{data.url}</a></p>
+      <p>{data.created_at}</p>
+      <p>{data.id}</p>
+    </div>
+  );
 }
 
-async function getParams(params:Promise<params>) {
-  const gotParams = await params;
-  return gotParams.workSlug;
-}
-
-export default async function WorkPage({params}: {params:Promise<params>}) {
-  const gotParams = getParams(params);
-  return <>
-  <Suspense fallback={<p>fetching..</p>}>
-    <h1>{gotParams}</h1>
-  </Suspense>
-  </>
+export default async function WorkPage({ params }: PageProps) {
+  return (
+    <Suspense fallback={<p>loading...</p>}>
+      <WorkData params={params} />
+    </Suspense>
+  );
 }
--- lib/getData.ts
+++ lib/getData.ts
@@ -2,11 +2,22 @@
 import { createClient } from "@/lib/supabase/server";
 import { jobTypeAll, workType, skillType } from "@/types/job.types";
 
-export  async function getDataFromSupabase<T = jobTypeAll | workType>(slug: string): Promise<T[]> {
+export async function getDataFromSupabase<T = jobTypeAll | workType>(slug: string): Promise<T[]> {
   const supabase = await createClient();
   const { data } = await supabase.from(slug).select();
   return (data || []) as T[];
 }
+
+export async function getEachDataFromSupabase<T = jobTypeAll | workType>(slug: string, slugFromParam: string): Promise<T | null> {
+  const supabase = await createClient();
+  const { data } = await supabase.from(slug).select().eq("slug", slugFromParam);
+  // Supabaseは配列を返すので、最初の要素を取得
+  if (data && data.length > 0) {
+    return data[0] as T;
+  }
+  return null;
+}
+
 
 export  async function getSkillsDataFromSupabase<T = skillType>(slug: string, filterName: string = "",filterValue: string = ""): Promise<T[]> {
   const supabase = await createClient();
